8759cf156808c5af40651f54dda87f7097a0b2b2
Your Name <your-email@example.com>
2026-08-12 00:45:10 +0300

    Fix Java syntax errors and update project setup

---
+++ 01-java-basics/01-Variables/main.java
@@ -0,0 +1,15 @@
+public class main {
+  public static void main(String[] args){
+
+  String name = "Mohamed";
+  int age = 19;
+  double gpa = 2.84;
+  boolean student = true;
+
+  System.out.println(name);
+  System.out.println(age);
+  System.out.println(gpa);
+  System.out.println(student);
+  
+  }
+}
